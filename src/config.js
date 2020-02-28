3cfb9769e482f167f5accf1aaf39e461d523110d
Dag Ivarsoy <dagivarsoy@gmail.com>
2020-02-28 13:47:31 +0000

    Correct pool id

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ export default {
     cognito: {
       REGION: "eu-west-1",
       USER_POOL_ID: "eu-west-1_JzXtiq4he",
-      APP_CLIENT_ID: "Y690dftmg2t450g8rueghr6tt1t",
+      APP_CLIENT_ID: "690dftmg2t450g8rueghr6tt1t",
       IDENTITY_POOL_ID: "eu-west-1:151d9ead-e9f0-4421-bd28-bfeb5dc41e11"
     }
   };
