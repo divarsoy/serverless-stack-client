df579ac4f88d05a41941a00edcdb5a6854a6954e
Dag Ivarsoy <dagivarsoy@gmail.com>
2020-02-28 15:59:17 +0000

    Upload attachement to S3 when submitting a note

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
 import "./NewNote.css";
 
 export default function NewNote(props) {
@@ -32,7 +33,10 @@ export default function NewNote(props) {
     setIsLoading(true);
   
     try {
-      await createNote({ content });
+      const attachment = file.current
+        ? await s3Upload(file.current)
+        : null;
+      await createNote({ content, attachment });
       props.history.push("/");
     } catch (e) {
       alert(e);
