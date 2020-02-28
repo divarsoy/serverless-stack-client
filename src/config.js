fd1e1fec05422b3015af322c658e26d5db5d11b8
Dag Ivarsoy <dagivarsoy@gmail.com>
2020-02-28 13:26:47 +0000

    Setup amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "ivarsoyfoto-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://a8h2yshkek.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_JzXtiq4he",
+      APP_CLIENT_ID: "Y690dftmg2t450g8rueghr6tt1t",
+      IDENTITY_POOL_ID: "eu-west-1:151d9ead-e9f0-4421-bd28-bfeb5dc41e11"
+    }
+  };
